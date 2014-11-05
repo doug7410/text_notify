9f6d50376c266b151e21724eb214a28281aaaf5f
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-05 03:06:11 +0000

    created some UI pages

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -2,9 +2,16 @@ class UiController < ApplicationController
   # before_filter do
   #   redirect_to :root if Rails.env.production?
   # end
-
-  layout "application"
+  layout 'application'
 
   def index
+  end
+
+  def sign_in
+    render :layout => false
+  end
+
+  def sign_up
+    render :layout => 'front_end'
   end
 end
