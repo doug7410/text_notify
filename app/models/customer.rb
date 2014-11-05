b90912a888fb800e49599f5f96f8672bdeb24ef3
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-05 20:44:20 +0000

    rspec and guard set up

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,3 @@
 class Customer < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name, :phone
+  validates_presence_of :first_name, :last_name, :phone_number
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105143709) do
+ActiveRecord::Schema.define(version: 20141105203349) do
+
+  create_table "customers", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "phone_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
