a54d3d81b99138ecd86cd2b68cf0cad387a669ae
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-04 22:56:32 -0500

    finished initial round of UI pages!

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -2,6 +2,8 @@ class UiController < ApplicationController
   # before_filter do
   #   redirect_to :root if Rails.env.production?
   # end
+  before_action :front_end_layout, only: [:front, :sign_up]
+
   layout 'application'
 
   def index
@@ -11,7 +13,9 @@ class UiController < ApplicationController
     render :layout => false
   end
 
-  def sign_up
+  private
+
+  def front_end_layout
     render :layout => 'front_end'
   end
 end
