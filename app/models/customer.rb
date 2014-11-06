3c627846fe2de89a58ff4829af2914d99067026c
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-06 20:21:19 +0000

    working on spec for customer phone number

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,9 @@
 class Customer < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :phone_number
+
+  validates_length_of :phone_number, minimum: 10  
+
+  def format_phone_number(phone_number)
+
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,4 +4,12 @@ describe Customer do
   it { should validate_presence_of(:first_name)}
   it { should validate_presence_of(:last_name)}
   it { should validate_presence_of(:phone_number)}
+  it { should ensure_length_of(:phone_number).is_at_least(10) }
+
+  describe "format_phone_number" do
+    it "should remove non-numeric characters" do
+      phone_number = "555-666-7777"
+      expect(Customer.format_phone_number(phone_number)).to eq('5556667777')
+    end
+  end
 end 
