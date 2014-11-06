5527e648170c239339a9c4701def3518172cd306
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-06 19:19:07 +0000

    refactor add_customer feature spec

--- spec/features/add_a_new_customer_spec.rb
+++ spec/features/add_a_new_customer_spec.rb
@@ -5,10 +5,7 @@ feature "Add A New customer" do
     sign_in_user
     visit new_customer_path
  
-    fill_in "First Name", with: "Freddy"
-    fill_in "Last Name", with: "Mercury"
-    fill_in "Phone Number", with: "555-555-5555" 
-    click_button "Save Customer"
+    fill_in_customer_form(first_name: 'Freddy', last_name: 'Mercury', phone_number: '555-555-5555')
     expect(page).to have_content("Freddy Mercury has been successfully added")
     expect(current_path).to eq(new_customer_path)
   end
@@ -16,14 +13,21 @@ feature "Add A New customer" do
   scenario "[a user adds a new customer with invalid info]" do
     sign_in_user
     visit new_customer_path
- 
-    fill_in "Last Name", with: "Mercury"
-    fill_in "Phone Number", with: "555-555-5555" 
-    click_button "Save Customer" 
+    
+    fill_in_customer_form()
     expect(page).to have_content("Please fix the errors below.")
     expect(page).to have_content("First name can't be blank")  
+    expect(page).to have_content("Last name can't be blank")  
+    expect(page).to have_content("Phone number can't be blank")  
 
     # can you test that a template is rendered here
     # expect(view).to render_template(:new)
   end
+end
+
+def fill_in_customer_form(options={})
+    fill_in "First Name", with: options[:first_name]
+    fill_in "Last Name", with: options[:last_name]
+    fill_in "Phone Number", with: options[:phone_number] 
+    click_button "Save Customer"
 end
