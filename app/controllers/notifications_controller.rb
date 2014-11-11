3a3b6a459f0a1db8b0b6f2c73098af3fc7aafd1d
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-11 15:51:12 +0000

    moved twilio logic to notifications_controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -9,7 +9,7 @@ class NotificationsController < ApplicationController
     @notification = Notification.new(notification_params)
     if @notification.save
 
-      @notification.send_text_message 
+      send_text_message(@notification) 
  
       flash[:success] = "The message has been sent."
       redirect_to new_notification_path
@@ -18,7 +18,23 @@ class NotificationsController < ApplicationController
     end
   end
 
+private
+
   def notification_params 
     params.require(:notification).permit(:customer_id, :message)
+  end
+
+  def send_text_message(notification)
+    begin
+      client = Twilio::REST::Client.new(ENV['twilio_account_sid'], ENV['twilio_auth_token'])      
+      
+      client.account.messages.create({
+        :from => '+15619238682',  
+        :to => notification.customer.phone_number, 
+        :body => notification.message  
+      })
+    rescue Twilio::REST::RequestError => e
+      puts e.message
+    end
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,20 +3,4 @@ class Notification < ActiveRecord::Base
 
   validates_presence_of :customer, :message
 
-  def send_text_message
-    account_sid = ENV['twilio_account_sid'] 
-    auth_token = ENV['twilio_auth_token'] 
-    
-    begin
-      client = Twilio::REST::Client.new account_sid, auth_token      
-      
-      client.account.messages.create({
-        :from => '+15619238682',  
-        :to => self.customer.phone_number, 
-        :body => self.message  
-      })
-    rescue Twilio::REST::RequestError => e
-      puts e.message
-    end
-  end
 end
