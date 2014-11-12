8c77005c812a79e5e3c04c2bafefb6b15d033586
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-11 20:48:58 -0500

    change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
   
-  root to: "pages#front"
+  root to: "notifications#new"
 
   resources :customers
   resources :notifications, only: [:new, :create]
