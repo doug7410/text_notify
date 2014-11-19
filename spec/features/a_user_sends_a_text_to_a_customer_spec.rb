9277f8ee2c78f238c29aa1e7623dd51a27118296
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-19 18:30:07 +0000

    feature spec for creating am individual notification is working

--- spec/features/a_user_sends_a_text_to_a_customer_spec.rb
+++ spec/features/a_user_sends_a_text_to_a_customer_spec.rb
@@ -38,47 +38,12 @@ feature "Sent a text to an individual customer" do
     expect(page).to have_content("The 'To' number 5555555555 is not a valid phone number")
   end
 
-
-
-
-
-  scenario '[send a text without specifying a new customer and leaving new customer flields blank]'
-
-  # scenario "[a user adds a new customer with valid info]" do
-  #   sign_in_user
-  #   visit new_customer_path 
- 
-  #   fill_in_customer_form(first_name: 'Freddy', last_name: 'Mercury', phone_number: '5555555555')
-  #   click_button "Save Customer"
-    
-  #   expect(page).to have_content("Freddy Mercury has been successfully added")
-  #   expect(current_path).to eq(new_customer_path)
-  # end
-
-  # scenario "[a user adds a new customer with invalid info]" do
-  #   sign_in_user
-  #   visit new_customer_path
-    
-  #   fill_in_customer_form()
-  #   click_button "Save Customer"
-    
-  #   expect(page).to have_content("Please fix the errors below.")
-  #   expect(page).to have_content("First name can't be blank")  
-  #   expect(page).to have_content("Last name can't be blank")  
-  #   expect(page).to have_content("Phone number can't be blank")  
-
-  # end
-
-
-  # scenario "[a user adds a new customer with an invalid phone number]" do
-  #   sign_in_user
-  #   visit new_customer_path
-    
-  #   fill_in_customer_form(first_name: 'Freddy', last_name: 'Mercury', phone_number: '555555555')
-  #   click_button "Save Customer"
-    
-  #   expect(page).to have_content("Phone number is the wrong length (should be 10 characters)")  
-  # end
+  scenario '[send a text without specifying a new customer and leaving new customer flields blank]' do
+    visit notifications_path
+    fill_in_notification_form(message: "I'm a message!")
+    click_button "Send Notification" 
+    expect(page).to have_content("Customer can't be blank")
+  end
 end
 
 def fill_in_notification_form(options={})
