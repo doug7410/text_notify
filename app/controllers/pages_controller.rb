e309a0ad5c1a63121078ae36439e7c369f028eec
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-27 13:28:19 -0500

    lock down dashboard with devise and clean up devise forms

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,6 @@
 class PagesController < ApplicationController
   
-  before_filter :go_to_ui_pages, only: [:front]
-
-  def front
-    # render :layout => 'front_end'
-  end
-
-  private
+  before_filter :authenticate_user!, only: [:dashboard]
   
-  def go_to_ui_pages
-    redirect_to dashboard_path if user_signed_in?
-  end
+  
 end
