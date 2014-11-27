70a8985507469663ff92931a1860fa297c91b993
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-27 02:31:38 -0500

    add method for devise reset password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   helper_method :trigger_errors
   
   protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:reset_password) { |u| u.permit(:full_name, :email, :password, :password_confirmation) }
+  end
 
   def trigger_errors(object)
     object.valid?
