8243db5c091ac1f770b7ddcefd5bd39017ae8f53
Doug Steinberg <dstein-phins@hotmail.com>
2014-11-30 16:33:13 -0500

    set up sidekiq worker for sending a group notification

--- app/controllers/group_notifications_controller.rb
+++ app/controllers/group_notifications_controller.rb
@@ -6,9 +6,10 @@ class GroupNotificationsController < ApplicationController
     @group_notification = GroupNotification.create(params.require(:group_notification).permit(:group_id, :group_message))
 
     if @group_notification.valid?
+      @group_notification.save
+      GroupNotificationSenderWorker.perform_async(@group_notification.id, current_user.id)
       flash[:success] = "A text has been successfully sent to the \"#{@group_notification.group.name}\" group."
       redirect_to notifications_path
-      send_text_to_each_customer_in_group
     else
       @notification = Notification.new
       @customer = Customer.new
@@ -19,13 +20,7 @@ class GroupNotificationsController < ApplicationController
 
 private 
 
-  def send_text_to_each_customer_in_group
-    @group_notification.group.customers.each do |customer|
-      notification = Notification.create(customer_id: customer.id, message: @group_notification.group_message, group_notification_id: @group_notification.id, user_id: current_user.id)
-        result = notification.send_text
-        notification.save_with_status(result)
-    end
-  end
+  
 
   
 end 
