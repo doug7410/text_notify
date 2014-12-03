ae24170a47380989c886d4d4339967b72091f649
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-03 15:44:55 +0000

    updated database

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,24 @@ ActiveRecord::Schema.define(version: 20141202224159) do
   add_index "business_owners", ["email"], name: "index_business_owners_on_email", unique: true
   add_index "business_owners", ["reset_password_token"], name: "index_business_owners_on_reset_password_token", unique: true
 
+  create_table "business_owsers", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "business_owsers", ["email"], name: "index_business_owsers_on_email", unique: true
+  add_index "business_owsers", ["reset_password_token"], name: "index_business_owsers_on_reset_password_token", unique: true
+
   create_table "customers", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
