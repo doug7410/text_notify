75c8ed8a9d1d6dcdaade47c956c4a5be6c3b92a4
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-05 17:42:44 +0000

    added twilio response test

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,7 +1,12 @@
 class SmsController < ApplicationController
     
   def create
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Hey Monkey. Thanks for the message!"
+    end
+    twiml.text
+    
     render nothing: true
   end
-  
+
 end 
