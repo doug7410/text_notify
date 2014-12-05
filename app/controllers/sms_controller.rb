22be107479820e40dd68d5f0a19d813ebe7f1c00
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-05 18:52:51 +0000

    redo sms controller and rout

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,11 +1,15 @@
-class SmsController
+class SmsController < ApplicationController
+  include Webhookable
+ 
+  after_filter :set_header
+ 
   skip_before_action :verify_authenticity_token
-    
-  def index
-    twiml = Twilio::TwiML::Response.new do |r|
+ 
+  def text
+    response = Twilio::TwiML::Response.new do |r|
       r.Message "Hey Monkey. Thanks for the message!"
     end
-    twiml.text
+ 
+    render_twiml response
   end
-
-end 
+end
