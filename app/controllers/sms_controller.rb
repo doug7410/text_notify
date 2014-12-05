835324f6971fb7c1c70bf24ab10e8fd01cbfd461
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-05 17:51:45 +0000

    skitp authenticity token

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,11 +1,12 @@
 class SmsController < ApplicationController
+  skip_before_action :verify_authenticity_token
     
   def create
     twiml = Twilio::TwiML::Response.new do |r|
       r.Message "Hey Monkey. Thanks for the message!"
     end
     twiml.text
-    
+
     render nothing: true
   end
 
