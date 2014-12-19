1fc2b1d2877f55b8c91430affbdde0d6992887f8
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-19 23:33:14 +0000

    notifications tabs and group notifications ajax

--- app/controllers/group_notifications_controller.rb
+++ app/controllers/group_notifications_controller.rb
@@ -3,17 +3,21 @@ class GroupNotificationsController < ApplicationController
 
 
   def create
-    @group_notification = GroupNotification.create(params.require(:group_notification).permit(:group_id, :group_message).merge(business_owner_id: current_business_owner.id))
-    if @group_notification.valid?
-      @group_notification.save
-      GroupNotificationSenderWorker.perform_async(@group_notification.id, current_business_owner.id)
-      flash[:success] = "A text has been successfully sent to the \"#{@group_notification.group.name}\" group."
-      redirect_to notifications_path
-    else
-      @notification = Notification.new
-      @customer = Customer.new
-      flash[:error] = "There was a problem."
-      render 'notifications/index'
+    respond_to do |format|
+      format.js do
+        @group_notification = GroupNotification.create(params.require(:group_notification).permit(:group_id, :group_message).merge(business_owner_id: current_business_owner.id))
+        if @group_notification.valid?
+          @group_notification.save
+          GroupNotificationSenderWorker.perform_async(@group_notification.id, current_business_owner.id)
+          flash[:success] = "A text has been successfully sent to the \"#{@group_notification.group.name}\" group."
+          render 'notifications/group'
+        else
+          @notification = Notification.new
+          @customer = Customer.new
+          flash[:error] = "There was a problem."
+          render 'notifications/group'
+        end
+      end
     end
   end
 
