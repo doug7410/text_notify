254010fdba8d2a937c48c131a4d255ee1dd81328
Doug Steinberg <dstein-phins@hotmail.com>
2014-12-21 16:08:23 -0500

    added validation to account settings

--- app/controllers/account_settings_controller.rb
+++ app/controllers/account_settings_controller.rb
@@ -21,10 +21,10 @@ class AccountSettingsController < ApplicationController
   end
 
   def update
-    account_settings = current_business_owner.account_setting
+    @account_settings = current_business_owner.account_setting
 
 
-    if account_settings.update(account_settings_params)
+    if @account_settings.update(account_settings_params)
       flash[:success] = "your settings have been updated"
       redirect_to account_settings_path
     else
---
+++ spec/models/account_setting_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe AccountSetting do
+  it { should validate_presence_of(:default_send_now_message)}
+  it { should validate_presence_of(:default_add_to_queue_message)}
+  it { should validate_presence_of(:default_send_from_queue_message)}
+end
