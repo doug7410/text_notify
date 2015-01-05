d63c662b7958ead4b8dd14a3f5469413677de218
Doug Steinberg <dstein-phins@hotmail.com>
2015-01-04 20:26:51 -0500

    view customers spec is passing

--- spec/features/view_customers_spec.rb
+++ spec/features/view_customers_spec.rb
@@ -1,20 +1,30 @@
 require 'spec_helper'
 include Warden::Test::Helpers
 Warden.test_mode!
-  
-feature "[View the customers in the database]" do
-  let(:doug) { Fabricate(:business_owner)}
-  background {login_as doug, scope: :business_owner, run_callbacks: false}
 
-  scenario "a business_owner views the customers" do
-    bob = Fabricate(:customer, first_name: "Bob", last_name: "Smith", phone_number: "5554447777", business_owner: doug)
-    jane = Fabricate(:customer, first_name: "Jane", last_name: "Doe", phone_number: "5554449999", business_owner: doug)
- 
-    visit customers_path 
+feature '[View the customers in the database]' do
+  let(:doug) { Fabricate(:business_owner) }
+  background { login_as doug, scope: :business_owner, run_callbacks: false }
 
-    expect(page).to have_content("Bob Smith")
-    expect(page).to have_content("(555)444-7777")
-    expect(page).to have_content("Jane Doe")
-    expect(page).to have_content("(555)444-9999")
+  scenario '[a business_owner views the customers]' do
+    Fabricate(
+      :customer,
+      full_name: 'Bob Smith',
+      phone_number: '5554447777',
+      business_owner: doug
+    )
+
+    Fabricate(
+      :customer,
+      full_name: 'Jane Doe',
+      phone_number: '5554449999',
+      business_owner: doug
+    )
+    visit customers_path
+
+    expect(page).to have_content('Bob Smith')
+    expect(page).to have_content('5554447777')
+    expect(page).to have_content('Jane Doe')
+    expect(page).to have_content('5554449999')
   end
 end
