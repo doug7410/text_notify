0433bc726b912257626545698d4d351ebbb6e297
Doug Steinberg <dstein-phins@hotmail.com>
2015-01-07 19:37:04 +0000

    set default order for business_owner_list

--- app/controllers/admin/business_owners_controller.rb
+++ app/controllers/admin/business_owners_controller.rb
@@ -1,5 +1,5 @@
 class Admin::BusinessOwnersController < AdminsController
   def index
-    @business_owners = BusinessOwner.all_except(current_business_owner)
+    @business_owners = BusinessOwner.order(created_at: :desc).all_except(current_business_owner)
   end
 end 
