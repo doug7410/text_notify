3433b9ced1c3c5569b1e11e3fdcc65047fbc99f2
Doug Steinberg <dstein-phins@hotmail.com>
2015-05-20 21:03:39 -0400

    comment out mailer from sms controller

--- app/workers/group_notification_sender_worker.rb
+++ app/workers/group_notification_sender_worker.rb
@@ -5,7 +5,12 @@ class GroupNotificationSenderWorker
     group_notification = GroupNotification.find(group_notification_id)
 
     group_notification.group.customers.each do |customer|
-      notification = Notification.create(customer_id: customer.id, message: group_notification.group_message, group_notification_id: group_notification.id, business_owner_id: business_owner_id)
+      notification = Notification.create(
+          customer_id: customer.id,
+          message: group_notification.group_message,
+          group_notification_id: group_notification.id,
+          business_owner_id: business_owner_id
+      )
         result = notification.send_text
         notification.save_with_status(result)
     end
